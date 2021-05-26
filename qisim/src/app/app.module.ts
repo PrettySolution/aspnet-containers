08aedaebab7367fcd103570eabf1734cfc5fbe88
Vasyl Herman PC W10 <vasil.german@gmail.com>
2021-05-26 13:13:43 +0300

    update

--- qisim/src/app/app.module.ts
+++ qisim/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {MsalModule} from '@azure/msal-angular';
 
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/v1-v2-upgrade-guide.md
+    MsalModule.forRoot(
+      {},
+      {},
+      {}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
